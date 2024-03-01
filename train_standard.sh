18fc2417d8045fc85cafba1b7843dfe44ad7eb4c
yagi <sensuiyagi2001@gmail.com>
2024-03-01 10:53:47 +0900

    a

--- train_standard.sh
+++ train_standard.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p checkpoints
-python -u train.py --name raft-things --stage things --validation sintel --restore_ckpt checkpoints/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 120 160 --wdecay 0.0001
+python -u core/train.py --name raft-things --stage things --validation sintel --restore_ckpt checkpoints/raft-chairs.pth --gpus 0 1 --num_steps 100000 --batch_size 6 --lr 0.000125 --image_size 120 160 --wdecay 0.0001
